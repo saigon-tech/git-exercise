788d47ff2ed77da063ffed355a42605af8a55a56
tcthinh.sgt <tcthinh.sgt@gmail.com>
2021-08-23 17:43:16 +0700

    Fix bug 2

--- process.php
+++ process.php
@@ -1,10 +1,6 @@
 <?php
     echo 'fix process 1';
-<<<<<<< HEAD
     echo 'fix process 2';
-=======
-    echo 'process 2';
->>>>>>> 80a38fd (Fix bug 1)
     echo 'process 3';
     echo 'process 4';
 ?>
